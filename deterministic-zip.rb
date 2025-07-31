4c5cfad3d9c151ac4d81e7968b03dfa95ce5e3d5
freakone <freakone@users.noreply.github.com>
2025-07-31 11:13:18 +0200

    change repo

--- deterministic-zip.rb
+++ deterministic-zip.rb
@@ -2,7 +2,7 @@
 
 class DeterministicZip < Formula
     desc "Simple (almost drop-in) replacement for zip that produces deterministic files."
-    url "https://github.com/timo-reymann/deterministic-zip/releases/download/4.0.1/deterministic-zip_darwin-amd64"
+    url "https://github.com/rhizo-co/deterministic-zip/releases/download/4.0.1/deterministic-zip_darwin-amd64"
     sha256 "1dfece8f0eaf249c98267a525a6033f0d67064141fef17f9dfd2215646c71aa9"
     version "4.0.1"
 
